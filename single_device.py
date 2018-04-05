0ff3a82d181a36ff1a54be885ffd4533253ddf1b
Peer David <david.peer.1991@gmail.com>
2018-04-05 15:55:13 +0200

    dded single_device.py

---
+++ single_device.py
@@ -0,0 +1,18 @@
+import numpy as np
+import tensorflow as tf
+
+# Build computation graph
+x = tf.placeholder(tf.int32, name="x")
+y = tf.constant(5, name="y")
+z = x * y
+
+# Start computation
+with tf.Session() as session:
+    writer = tf.summary.FileWriter('logs', session.graph)
+    input_data = { x: 7 }
+    result = session.run(z, input_data)
+    print("Result: %d" % result)
+    writer.close()
+    
+    #writer = tf.summary.FileWriter('logs', session.graph)
+    #writer.close()
